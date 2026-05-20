6db3b196d020bb2dd9e51c262836ce8b3dd5c13c
Spraggett77 <andrewspraggett66@gmail.com>
2026-05-20 00:25:23 -0400

    Finished Repl logic and added help and exit commands

--- main.go
+++ main.go
@@ -6,6 +6,48 @@ import (
 	"os"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand { // Storing commands in a map that we can access via this func
+	//Helps get past the cycle initialization of var names the callback func.
+
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandhelp,
+		},
+	}
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+
+	return nil
+
+}
+
+func commandhelp() error {
+	fmt.Println("Welcome to the Pokedex!\nUsage:\n")
+
+	for key, value := range getCommands() {
+		fmt.Printf("%s: %s\n", key, value.description)
+
+	}
+	return nil
+}
+
 func main() {
 
 	scanner := bufio.NewScanner(os.Stdin)
@@ -18,17 +60,25 @@ func main() {
 			break
 
 		}
+
 		userInput := scanner.Text()
 
 		cleanedInput := cleanInput(userInput)
-
-		command := cleanedInput[0]
-
-		if len(command) == 0 {
+		if len(cleanedInput) == 0 {
 			continue
 		}
+		command := cleanedInput[0]
 
-		fmt.Printf("Your command was: %s\n", command)
+		cmd, exists := getCommands()[command]
+		if exists {
+			err := cmd.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+
+		} else {
+			fmt.Println("Unknown command")
+		}
 
 	}
 
